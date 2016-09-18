41b6cf4846a1688db5fff992f21042466333e20e
suiyujin <suiyujin04@gmail.com>
2016-09-18 12:30:34 +0900

    unix domain socket

--- webapp/ruby/config/unicorn/isuda.rb
+++ webapp/ruby/config/unicorn/isuda.rb
@@ -4,3 +4,5 @@ timeout 120
 
 stderr_path "#{File.dirname(__FILE__)}/logs/unicorn_isuda_err.log"
 stdout_path "#{File.dirname(__FILE__)}/logs/unicorn_isuda_out.log"
+
+listen "/tmp/unicorn_isuda.sock", :backlog => 2048
--- webapp/ruby/config/unicorn/isutar.rb
+++ webapp/ruby/config/unicorn/isutar.rb
@@ -4,3 +4,5 @@ timeout 120
 
 stderr_path "#{File.dirname(__FILE__)}/logs/unicorn_isutar_err.log"
 stdout_path "#{File.dirname(__FILE__)}/logs/unicorn_isutar_out.log"
+
+listen "/tmp/unicorn_isutar.sock", :backlog => 2048
