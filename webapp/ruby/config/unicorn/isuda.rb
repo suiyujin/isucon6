a08604d5eeeb0be66527ebcebcc2e150f464ef24
suiyujin <suiyujin04@gmail.com>
2016-09-18 11:43:45 +0900

    write unicorn logs

--- webapp/ruby/config/unicorn/isuda.rb
+++ webapp/ruby/config/unicorn/isuda.rb
@@ -1,3 +1,6 @@
 worker_processes 5
 preload_app true
 timeout 120
+
+stderr_path "#{File.dirname(__FILE__)}/logs/unicorn_isuda_err.log"
+stdout_path "#{File.dirname(__FILE__)}/logs/unicorn_isuda_out.log"
